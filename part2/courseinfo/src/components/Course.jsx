04da8e941f37dcc3a90d21ea973f9f7ad692ba89
ilhankalac <ilhandeveloper@gmail.com>
2025-12-25 13:47:13 +0100

    feat(course-info): 2.2: Course information step 7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -5,8 +5,10 @@ const Course = ({ course }) => {
       {course.parts.map(part => (
         <p key={part.id}>{part.name} { part.exercises }</p>
       ))}
+      <strong>
+        total of { course.parts.reduce((sum, part) => sum + part.exercises,0)}
+      </strong>
     </div>
   )
 }
-
 export default Course
