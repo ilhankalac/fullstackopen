2adc025ca703461768f51bb7b01700e7165587bf
ilhankalac <ilhandeveloper@gmail.com>
2025-12-30 23:26:45 +0100

    feat(phonebook): 2.18* Data for countries, step 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,19 +3,38 @@ import countriesService from "./services/countries"
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('')
+  const [countries, setCountries] = useState([])
 
   const handleSearchInput = (event) => {
     setSearchValue(event.target.value)
   }
+
+  const searchByCountryName = () => {
+  }
   
   useEffect(() => {
     countriesService.getAll().then((data) => {
-      console.log(data);
+      const cleanData = data.map((country) => ({
+        name: country.name?.official || '',
+        capital: country.capital || [],
+        area: country.area || 0,
+        languages: country.languages || {}
+      }))
+      setCountries(countries.concat(cleanData))
     })
+
   }, [])
+
   return (
     <div>
-      find countries <input value={searchValue} onChange={handleSearchInput}/>
+      find countries <input value={searchValue} onChange={handleSearchInput} />
+      <div>
+        {
+          countries.map(country => 
+            <div key={country.name}>{country.name}</div>
+          )
+        }
+      </div>
     </div>
   )
 }
