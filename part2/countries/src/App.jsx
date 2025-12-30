c888258a546518922f86ea7e01962370dec8ab97
ilhankalac <ilhandeveloper@gmail.com>
2025-12-30 23:09:12 +0100

    feat(phonebook): 2.18* Data for countries, step 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+import countriesService from "./services/countries"
+
 const App = () => {
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchInput = (event) => {
+    setSearchValue(event.target.value)
+  }
+  
+  useEffect(() => {
+    countriesService.getAll().then((data) => {
+      console.log(data);
+    })
+  }, [])
   return (
     <div>
-      <p>Hello world</p>
+      find countries <input value={searchValue} onChange={handleSearchInput}/>
     </div>
   )
 }
