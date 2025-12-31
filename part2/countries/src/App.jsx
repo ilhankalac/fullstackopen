a51c9b05330524cc495921d63746e09f08d1f7bf
ilhankalac <ilhandeveloper@gmail.com>
2025-12-31 17:44:57 +0100

    feat(phonebook): 2.18* Data for countries, step 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,36 +4,48 @@ import countriesService from "./services/countries"
 const App = () => {
   const [searchValue, setSearchValue] = useState('')
   const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState([])
 
   const handleSearchInput = (event) => {
-    setSearchValue(event.target.value)
+    const value = event.target.value
+    setSearchValue(value)
+    searchCountries(value)
   }
 
-  const searchByCountryName = () => {
+  const searchCountries = (value) => {
+    const q = value.toLowerCase()
+    const results = allCountries.filter(country =>
+      country.name.toLowerCase().includes(q)
+    )
+    setCountries(results)
   }
-  
+
   useEffect(() => {
     countriesService.getAll().then((data) => {
       const cleanData = data.map((country) => ({
-        name: country.name?.official || '',
+        name: country?.name?.common || '',
         capital: country.capital || [],
         area: country.area || 0,
-        languages: country.languages || {}
+        languages: country.languages || {},
+        id: country.cca3 
       }))
-      setCountries(countries.concat(cleanData))
+      setCountries(cleanData)
+      setAllCountries(cleanData)
     })
-
   }, [])
 
   return (
     <div>
       find countries <input value={searchValue} onChange={handleSearchInput} />
       <div>
-        {
-          countries.map(country => 
-            <div key={country.name}>{country.name}</div>
-          )
+        {searchValue.length > 0 && countries.length < 10 &&
+          countries.map(country => (
+            <div key={country.id}>{country.name}</div>
+          ))
         }
+        {searchValue.length > 0 && countries.length >= 10 && (
+          <div>Too many matches, specify another filter</div>
+        )}
       </div>
     </div>
   )
