33573fdc0571ce1d35bc6922ab185e985c79b435
ilhankalac <ilhandeveloper@gmail.com>
2026-01-01 23:33:15 +0100

    feat(phonebook): 2.18* Data for countries, step 1

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/'
 
 const getAll = () => {
-  const request = axios.get(baseUrl+ '/all')
+  const request = axios.get(baseUrl+ 'all')
   return request.then(response => response.data)
 }
 
+const getByName = (countryName) => {
+  const request = axios.get(baseUrl + 'name/' + countryName )
+  return request.then(response => response.data)
+}
 
-export default { getAll }
+export default { getAll, getByName }
