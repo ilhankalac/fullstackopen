e74bd36f4d273b1882f2de0541c15c8ead02c231
ilhankalac <ilhandeveloper@gmail.com>
2026-01-06 17:53:20 +0100

    feat(phonebook): 3.2: Phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,30 +2,42 @@ const express = require('express')
 const app = express()
 
 const persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const count = persons.length
+  const time = new Date()
+  
+  response.send(`
+    <div>
+      <p>Phonebook has info for ${count} people</p>
+      <p>${time}</p>
+    </div>
+  `)
 })
 
 const PORT = 3001
