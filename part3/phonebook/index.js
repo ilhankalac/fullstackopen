decc712253938ce9b642858037b622482fb23563
ilhankalac <ilhandeveloper@gmail.com>
2026-01-06 18:23:19 +0100

    feat(phonebook): 3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const persons = [
   { 
@@ -53,8 +54,32 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  notes = persons.filter(person => person.id !== id)
+  persons = persons.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1_000_000_000)
+}
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number:  body.number
+  }
+  
+  persons.push(person)
+  response.json(person)
 })
 
 const PORT = 3001
