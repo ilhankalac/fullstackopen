f94a7aa0f686b0b1437ad6856e036fd7d213fdb3
ilhankalac <ilhandeveloper@gmail.com>
2026-01-09 14:13:05 +0100

    feat(phonebook): 3.12: Command-line database, update mongo.js to handle contact input and display phonebook

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -5,9 +5,14 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
+if (process.argv.length === 4) {
+  console.log('give the contact number');
+  process.exit(1)
+}
+
 const password = process.argv[2]
 
-const url = `mongodb+srv://filmrealist_db_user:${password}@cluster0.v5v5ncz.mongodb.net/?appName=Cluster0`
+const url = `mongodb+srv://filmrealist_db_user:${password}@cluster0.v5v5ncz.mongodb.net/phonebook?appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -17,15 +22,25 @@ const contactSchema = new mongoose.Schema({
   name: String,
   number: String
 })
-
 const Contact = mongoose.model('Contact', contactSchema)
 
 const contact = new Contact({
-  name: 'Laganica',
-  number: 'totalna'
+  name: process.argv[3] || '',
+  number: process.argv[4] || ''
 })
 
-contact.save().then(result => {
-  console.log('contact saved!')
-  mongoose.connection.close()
-})
+if (contact.name && contact.number) {
+  contact.save().then(result => {
+    console.log(`added ${contact.name} number ${contact.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Contact.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(contact => {
+      console.log(`${contact.name} ${contact.number}`);
+    });
+    mongoose.connection.close()
+  })
+}
+
