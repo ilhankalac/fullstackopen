6c09d38f044983dcef14fc87350a3870e72df647
ilhankalac <ilhandeveloper@gmail.com>
2026-01-09 16:27:26 +0100

    feat(phonebook): 3.13: Phonebook database, step 1, add dotenv for environment variable management and update MongoDB connection

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,6 +3,13 @@ const app = express()
 app.use(express.json())
 var morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
+mongoose.connect(url, { family: 4 })
+
 app.use(express.static('dist'))
 
 app.use(cors())
@@ -18,31 +25,17 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
 app.get('/info', (request, response) => {
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -6,7 +6,7 @@ if (process.argv.length < 3) {
 }
 
 if (process.argv.length === 4) {
-  console.log('give the contact number');
+  console.log('give the contact number')
   process.exit(1)
 }
 
@@ -14,33 +14,32 @@ const password = process.argv[2]
 
 const url = `mongodb+srv://filmrealist_db_user:${password}@cluster0.v5v5ncz.mongodb.net/phonebook?appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-
+mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
 
-const contactSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   number: String
 })
-const Contact = mongoose.model('Contact', contactSchema)
 
-const contact = new Contact({
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
   name: process.argv[3] || '',
   number: process.argv[4] || ''
 })
 
-if (contact.name && contact.number) {
-  contact.save().then(result => {
-    console.log(`added ${contact.name} number ${contact.number} to phonebook`)
+if (person.name && person.number) {
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
     mongoose.connection.close()
   })
 } else {
-  Contact.find({}).then(result => {
-    console.log('phonebook:');
-    result.forEach(contact => {
-      console.log(`${contact.name} ${contact.number}`);
-    });
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(p => {
+      console.log(`${p.name} ${p.number}`)
+    })
     mongoose.connection.close()
   })
 }
-
