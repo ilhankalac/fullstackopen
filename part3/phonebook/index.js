4766ffd5428795746a26e6e348a939623735c997
ilhankalac <ilhandeveloper@gmail.com>
2026-01-10 15:19:35 +0100

    feat(phonebook): 3.15: Phonebook database, step 3, update delete person endpoint to use mongoose for deletion

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -51,8 +51,10 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndDelete(id).then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 const generateId = () => {
