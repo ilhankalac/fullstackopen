c0be86321257fa08b394f90881da47d879c4658d
ilhankalac <ilhandeveloper@gmail.com>
2026-01-15 17:37:49 +0100

    feat(bloglist): 4.1 Blog List, step 1, add dependencies for dotenv, express, and mongoose in package.json

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,10 +1,42 @@
-const http = require('http')
+const express = require('express')
+const mongoose = require('mongoose')
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+require('dotenv').config()
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const Blog = mongoose.model('Blog', blogSchema)
+const password = process.argv[2]
+
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
