80ad8b679553edf3e662cd6a5a6929a76701896f
ilhankalac <ilhandeveloper@gmail.com>
2026-01-24 17:09:13 +0100

    feat(bloglist): 4.4: Helper Functions and Unit Tests, step 2, add tests for totalLikes function in list_helper, covering single blog and empty list scenarios

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -10,6 +10,21 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
   const blogs = [
     {
       _id: "5a422a851b54a676234d17f7",
@@ -64,5 +79,10 @@ describe('total likes', () => {
   test('this list should return 36 total likes', () => {
     const result = listHelper.totalLikes(blogs)
     assert.strictEqual(result, 36)
+  })
+
+  test('this should return 0 total likes', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
   })
 })
