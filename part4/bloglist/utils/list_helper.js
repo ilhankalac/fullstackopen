d9476ba75f267dd910262b6f64700760be13a7aa
ilhankalac <ilhandeveloper@gmail.com>
2026-01-25 22:42:25 +0100

    feat(bloglist): 4.5*: Helper Functions and Unit Tests, step 3, add favoriteBlog function and corresponding tests for blog utilities

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,18 @@ const totalLikes = (blogs) => {
   }, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  
+  return blogs.reduce((favorite, current) => {
+    return (current.likes > favorite.likes) ? current : favorite;
+  });
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
