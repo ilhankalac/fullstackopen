e490ac7ef586984d11fe9f512479f6d762d116b4
ilhankalac <ilhandeveloper@gmail.com>
2026-04-05 14:59:56 +0200

    feat(bloglist): 4.8: Blog List Tests, step 1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,50 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Author One',
+    url: 'http://example.com/first',
+    likes: 5
+  },
+  {
+    title: 'Second blog', 
+    author: 'Author Two',
+    url: 'http://example.com/second',
+    likes: 3
+  },
+  {
+    title: 'Third blog',
+    author: 'Author Three',
+    url: 'http://example.com/third',
+    likes: 8
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blogs is returned', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
