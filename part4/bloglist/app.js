06f6b43662a6d5faa553f3b9524a5c9f8896348a
ilhankalac <ilhandeveloper@gmail.com>
2026-04-12 16:35:29 +0200

    feat(bloglist): 4.15: Blog List Expansion, step 3, add users router with GET and POST endpoints

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const { username, name, password } = req.body
+
+    if (!password || password.length < 3) {
+      return res.status(400).json({
+        error: 'password must be at least 3 characters long'
+      })
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const savedUser = await new User({
+      username,
+      name,
+      passwordHash
+    }).save()
+
+    res.status(201).json(savedUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = usersRouter
