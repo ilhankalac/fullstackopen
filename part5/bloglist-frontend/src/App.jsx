07fa04d812cbbce76a9166698ff2d1eb818886d7
ilhankalac <ilhandeveloper@gmail.com>
2026-06-03 14:29:45 +0200

    feat(bloglist-frontend): 5.1: Blog List Frontend, step 1, wip

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import Login from './components/Login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Login />
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/components/Login.jsx
@@ -0,0 +1,20 @@
+const Login = () => {
+  return (
+    <div>
+      <h2>log in to application</h2>
+      <form>
+        <div>
+          username
+            <input />
+        </div>
+        <div>
+          password
+            <input />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
