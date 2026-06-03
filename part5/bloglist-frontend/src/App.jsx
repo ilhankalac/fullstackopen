94e10961d45ec430ea9f63d1ed609f506bbaa877
ilhankalac <ilhandeveloper@gmail.com>
2026-06-03 16:11:18 +0200

    feat(blostlist-frontend): 5.1: Blog List Frontend, step 1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,17 +5,20 @@ import Login from './components/Login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
-
+  if (user === null) {
+    return <Login setUser={setUser} />
+  }
   return (
     <div>
       <h2>blogs</h2>
-      <Login />
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/bloglist-frontend/src/components/Login.jsx
+++ part5/bloglist-frontend/src/components/Login.jsx
@@ -1,15 +1,49 @@
-const Login = () => {
+import { useState } from 'react'
+import loginService from '../services/login'
+
+const Login = ({ setUser }) => {
+
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('logged in user:', user)
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
   return (
     <div>
       <h2>log in to application</h2>
-      <form>
+      <form onSubmit={handleLogin}>
         <div>
-          username
-            <input />
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
         </div>
         <div>
-          password
-            <input />
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
         </div>
         <button type="submit">login</button>
       </form>
